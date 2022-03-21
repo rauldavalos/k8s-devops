583066c88b02c61f20b8d033d34d8dd3fd22367a
Raul Davalos <raul.davalos@emc.com>
2022-03-21 14:23:10 -0400

    Adding canary and spring-music helm chart

---
+++ spring-music/help.sh
@@ -0,0 +1,4 @@
+echo "To install chart:"
+echo "   helm install spring-music ./spring-music --set cluster=local"
+echo "   kubectl port-forward service/spring-music 8080:80"
+echo "Browse http://127.0.0.1.nip.io:8080/"
